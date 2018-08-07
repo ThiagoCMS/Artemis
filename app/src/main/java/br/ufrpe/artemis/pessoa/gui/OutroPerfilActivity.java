73a725def963665b6e1f17dfc7c2311eed1147e2
Bianca Cardoso <biigue@live.com>
2018-08-07 03:17:57 -0300

    Outro perfil anuncios

--- app/src/main/java/br/ufrpe/artemis/pessoa/gui/OutroPerfilActivity.java
+++ app/src/main/java/br/ufrpe/artemis/pessoa/gui/OutroPerfilActivity.java
@@ -1,7 +1,9 @@
 package br.ufrpe.artemis.pessoa.gui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,6 +15,7 @@ import br.ufrpe.artemis.avaliacao.negocio.AvaliacaoNegocio;
 import br.ufrpe.artemis.pessoa.dominio.Pessoa;
 import br.ufrpe.artemis.pessoa.negocio.PessoaNegocio;
 import br.ufrpe.artemis.R;
+import br.ufrpe.artemis.servico.gui.ServicoOutraPessoaActivity;
 
 public class OutroPerfilActivity extends AppCompatActivity {
     private TextView nome;
@@ -25,13 +28,19 @@ public class OutroPerfilActivity extends AppCompatActivity {
     private TextView ntPreço;
     private TextView ntQualidade;
     private TextView ntAtendimento;
-
+    private Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outro_perfil);
         setTela();
+        botaoAnuncios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                anuncios();
+            }
+        });
     }
 
     private void setTela(){
@@ -52,7 +61,7 @@ public class OutroPerfilActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         int idUsuario = extras.getInt("id");
         PessoaNegocio pessoaNegocio = new PessoaNegocio();
-        Pessoa pessoa = pessoaNegocio.recuperarPessoaPorUsuario(idUsuario);
+        pessoa = pessoaNegocio.recuperarPessoaPorUsuario(idUsuario);
         AvaliacaoNegocio avaliacaoNegocio = new AvaliacaoNegocio();
         Classificacao classificacao = avaliacaoNegocio.notasPrestadora(idUsuario);
         nome.setText(pessoa.getNome());
@@ -64,5 +73,9 @@ public class OutroPerfilActivity extends AppCompatActivity {
         ntPreço.setText("Preço - " +  df2.format(classificacao.getMediaPreco()));
         ntQualidade.setText("Qualidade - " +  df2.format(classificacao.getMediaQualidade()));
         ntAtendimento.setText("Atendimento - " +  df2.format(classificacao.getMediaAtendimento()));
+    }
+
+    public void anuncios(){
+        startActivity(new Intent(OutroPerfilActivity.this, ServicoOutraPessoaActivity.class).putExtra("id", pessoa.getId()));
     }
 }
---
+++ app/src/main/java/br/ufrpe/artemis/servico/gui/ServicoOutraPessoaActivity.java
@@ -0,0 +1,50 @@
+package br.ufrpe.artemis.servico.gui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import br.ufrpe.artemis.R;
+import br.ufrpe.artemis.servico.dominio.Servico;
+import br.ufrpe.artemis.servico.negocio.ServicoNegocio;
+
+public class ServicoOutraPessoaActivity extends AppCompatActivity {
+    private ArrayList<Servico> arrayListServico;
+    private ListView listViewGeral;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servico_outra_pessoa);
+        setTela();
+        listViewGeral.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                escolherServico(position);
+            }
+        });
+    }
+
+    private void escolherServico(int position){
+        Servico servico = arrayListServico.get(position);
+        int idServico = servico.getId();
+        Intent intent = new Intent(ServicoOutraPessoaActivity.this, ServicoActivity.class);
+        intent.putExtra("id", String.valueOf(idServico));
+        startActivity(intent);
+    }
+
+    private void setTela() {
+        final ServicoNegocio negocio = new ServicoNegocio();
+        Bundle extras = getIntent().getExtras();
+        int id = extras.getInt("id");
+        arrayListServico = negocio.listarSevicosUs(id);
+        listViewGeral = findViewById(R.id.anunciosOutraId);
+        final ArrayAdapter servicoAdapter = new ServicoAdapter(arrayListServico);
+        listViewGeral.setAdapter(servicoAdapter);
+    }
+}
